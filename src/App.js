0258d00150f350fd0788f2904356c26f0615e091
Manish <i.m.manish100202@gmail.com>
2024-01-30 09:45:28 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navigation from "./Components/Navigation";
+import Hero from "./Components/Hero";
+
+function App() {
+  return (
+    <div>
+      <header><Navigation /></header>
+      <section><Hero/></section>
+    </div>
+  );
+}
+
+export default App;
